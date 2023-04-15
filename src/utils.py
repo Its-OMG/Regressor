7c28303b6ca83c6e402f514db3bb57203042901c
its-OMG <Gusainom77@gmail.com>
2023-04-15 16:29:10 +0530

    Prediction Pipeline and Flaskapp

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,8 @@ import pickle
 from src.exceptions import CustomException
 from src.logger import logging
 
+
+# Function to save Model as Pickle File
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -22,6 +24,9 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
+
+
+# Function for Evaluating the Models One by One
 def evaluate_model(X_train, y_train, X_test, y_test, models):
     
     report = {}
@@ -40,3 +45,15 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
         report[list(models.keys())[i]] = test_model_score
 
     return report
+
+
+
+# Function For loading a pickle File
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        logging.info("Error Occred While loading Pickle File")
+        raise CustomException(e, sys)
